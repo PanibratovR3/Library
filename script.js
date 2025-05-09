188025b54d6487c04cda06d7ac5b221c33f5396d
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-09 12:55:15 +0300

    Added function for creating Book object and adding it into arrray of books.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Test");
+const myLibrary = [];
+
+function Book(author, title, numberOfPages, isRead) {
+  this.id = crypto.randomUUID();
+  this.author = author;
+  this.title = title;
+  this.numberOfPages = numberOfPages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(author, title, numberOfPages, isRead) {
+  const newBook = new Book(author, title, numberOfPages, isRead);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("Kurt Vonneghut", "Hocus Pocus", 302, false);
+addBookToLibrary("Oscar Wilde", "The Pitcure of Dorian Gray", 287, true);
