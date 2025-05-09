f0ce7f7f339ff4f22be3a7eed81103919e2add57
PanibratovR3 <roman.panibratov@gmail.com>
2025-05-09 16:29:03 +0300

    Added function for adding book card, which contains all information and buttons for settings.

--- script.js
+++ script.js
@@ -13,5 +13,91 @@ function addBookToLibrary(author, title, numberOfPages, isRead) {
   myLibrary.push(newBook);
 }
 
-addBookToLibrary("Kurt Vonneghut", "Hocus Pocus", 302, false);
+function displayBook(book) {
+  const mainField = document.querySelector(".central-column");
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-item");
+  bookCard.setAttribute("data-id", book.id);
+  // Add author info
+  const authorRow = document.createElement("div");
+  authorRow.classList.add("book-item-row");
+  const authorLabel = document.createElement("div");
+  authorLabel.classList.add("label");
+  authorLabel.textContent = "Author";
+  authorRow.appendChild(authorLabel);
+  const authorValue = document.createElement("div");
+  authorValue.classList.add("book-author-value");
+  authorValue.textContent = book.author;
+  authorRow.appendChild(authorValue);
+  bookCard.appendChild(authorRow);
+  mainField.appendChild(bookCard);
+  // Add title info
+  const titleRow = document.createElement("div");
+  titleRow.classList.add("book-item-row");
+  const titleLabel = document.createElement("div");
+  titleLabel.classList.add("label");
+  titleLabel.textContent = "Title";
+  titleRow.appendChild(titleLabel);
+  const titleValue = document.createElement("div");
+  titleValue.classList.add("book-title-value");
+  titleValue.textContent = book.title;
+  titleRow.appendChild(titleValue);
+  bookCard.appendChild(titleRow);
+  // Add number of pages info
+  const numOfPagesRow = document.createElement("div");
+  numOfPagesRow.classList.add("book-item-row");
+  const numOfPagesLabel = document.createElement("div");
+  numOfPagesLabel.classList.add("label");
+  numOfPagesLabel.textContent = "Number of pages";
+  numOfPagesRow.appendChild(numOfPagesLabel);
+  const numOfPagesValue = document.createElement("div");
+  numOfPagesValue.classList.add("book-numOfPages-value");
+  numOfPagesValue.textContent = book.numberOfPages;
+  numOfPagesRow.appendChild(numOfPagesValue);
+  bookCard.appendChild(numOfPagesRow);
+  // Add reading status info
+  const isReadRow = document.createElement("div");
+  isReadRow.classList.add("book-item-row");
+  const isReadLabel = document.createElement("div");
+  isReadLabel.classList.add("label");
+  isReadLabel.textContent = "Has been read?";
+  isReadRow.appendChild(isReadLabel);
+  const isReadValue = document.createElement("div");
+  isReadValue.classList.add("book-isRead-value");
+  if (book.isRead) {
+    isReadValue.textContent = "Yes";
+    isReadValue.classList.add("yes");
+  } else {
+    isReadValue.textContent = "No";
+    isReadValue.classList.add("no");
+  }
+  isReadRow.appendChild(isReadValue);
+  bookCard.appendChild(isReadRow);
+  // Add buttons
+  const settingsRow = document.createElement("div");
+  settingsRow.classList.add("book-item-settings");
+  const buttonStatusToggle = document.createElement("button");
+  buttonStatusToggle.classList.add("book-setting");
+  buttonStatusToggle.classList.add("toggle");
+  buttonStatusToggle.textContent = "Toggle read state";
+  settingsRow.appendChild(buttonStatusToggle);
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("book-setting");
+  deleteButton.classList.add("delete");
+  deleteButton.textContent = "Delete";
+  settingsRow.appendChild(deleteButton);
+  bookCard.appendChild(settingsRow);
+  mainField.appendChild(bookCard);
+}
+
+addBookToLibrary("Kurt Vonneghut", "Hocus Pocus", 302, true);
 addBookToLibrary("Oscar Wilde", "The Pitcure of Dorian Gray", 287, true);
+addBookToLibrary(
+  "Hunther S. Thompson",
+  "Fear and Loathing in Las Vegas",
+  204,
+  false
+);
+for (const book of myLibrary) {
+  displayBook(book);
+}
